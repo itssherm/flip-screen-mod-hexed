f345f7f52cfbf7862180b59212d6113991375bc3
Snow <142698082+itssherm@users.noreply.github.com>
2024-06-15 22:36:51 -0400

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,26 @@
+﻿using CoreRuntime.Interfaces;
+using CoreRuntime.Manager;
+using FlipaScreen;
+using Il2CppSystem;
+using System.Collections;
+using UnityEngine;
+
+namespace Basse
+{
+    public class FlipScreen : HexedCheat
+    {
+        public override void OnLoad(string[] args)
+        {
+            MonoManager.PatchUpdate(typeof(VRCApplication).GetMethod(nameof(VRCApplication.Update)));
+            MonoManager.PatchOnApplicationQuit(typeof(VRCApplicationSetup).GetMethod(nameof(VRCApplicationSetup.OnApplicationQuit))); // Optional Hook to enable the OnApplicationQuit callback
+        }
+
+
+
+        public override void OnUpdate()
+        {
+            headflippa.Update();
+        }
+
+    }
+}
---
+++ headflippa.cs
@@ -0,0 +1,38 @@
+﻿using CoreRuntime.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FlipaScreen
+{
+    internal class headflippa : HexedCheat
+    {
+
+        public static void Update()
+        {
+            if (!Input.GetKey(KeyCode.LeftControl)) return;
+
+            Camera camera = Camera.main;
+            if (camera == null) return;
+
+            float mouseWheel = Input.GetAxis("Mouse ScrollWheel");
+            if (mouseWheel > 0.1)
+            {
+                Quaternion quaternion = camera.transform.rotation;
+                Vector3 euler = quaternion.eulerAngles;
+                euler -= Vector3.forward;
+                camera.transform.rotation = Quaternion.Euler(euler);
+            }
+            else if (mouseWheel < -0.1)
+            {
+                Quaternion quaternion = camera.transform.rotation;
+                Vector3 euler = quaternion.eulerAngles;
+                euler += Vector3.forward;
+                camera.transform.rotation = Quaternion.Euler(euler);
+            }
+        }
+    }
+}
